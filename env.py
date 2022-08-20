da4fa560aef808575389f2b99bdb3b6ca71bcb5b
IronXiao <xiaoleigs@gmail.com>
2022-08-20 17:14:50 +0800

    update unsave change

--- env.py
+++ env.py
@@ -21,19 +21,19 @@ MAX_FILTER_VALID_IP_COUNT = 2000
 MAX_FILTER_BETTER_IP_COUNT = 200
 # 测试下载连接超时，当前无效
 TEST_DOWNLOAD_CONNECTTIMEOUT = 1
-# 期望网速
+# 期望网速，当前无效
 EXPECTED_SPEED = 7000
-# 下载测试文件名
+# 下载测试文件名，当前无效
 TEST_DOWNLOAD_SAVE_FILE = 'down.bin'
-# 下载时使用的浏览器标识
+# 下载时使用的浏览器标识，当前无效
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-# 下载文件domain
+# 下载文件domain，当前无效
 TEST_DOWNLOAD_DOMAIN = 'cloudflaremirrors.com'
-# 下载文件的网络端口
+# 下载文件的网络端口，当前无效
 TEST_DOWNLOAD_DOMAIN_PORT = 443
-# 测试下载总时长
+# 测试下载总时长，当前无效
 TEST_DOWNLOAD_TIMEOUT = 20
-# 下载文件路径
+# 下载文件路径，当前无效
 TEST_DOWNLOAD_FILE_PATH = 'archlinux/iso/latest/archlinux-x86_64.iso'
-# 是否测试下载速度
+# 是否测试下载速度，请勿修改！！！
 TEST_DOWNLOAD_SPEED = False
